db8432e2680f2cb6a0c55413e670eb10b6daab2e
Kamen Angelov <kamen.angelov@telus.com>
2016-11-09 17:21:33 -0500

    Initial commit

---
+++ src/main/java/com/qualicom/rr/controller/MainController.java
@@ -0,0 +1,29 @@
+package com.qualicom.rr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by x110277 on 11/09/2016.
+ */
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String beginHere() {
+        return "redirect:main";
+    }
+
+    @RequestMapping(value = "/main", method = RequestMethod.GET)
+    public ModelAndView main() {
+        ModelAndView model = new ModelAndView();
+        model.addObject("title","Qualicom Report Runner");
+        model.addObject("message","Nothing here yet.");
+        model.setViewName("main");
+        return model;
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/main.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: x110277
+  Date: 11/09/2016
+  Time: 02:41 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<html>
+<head>
+    <title>${title}</title>
+</head>
+<body>
+    <h1>${title}</h1>
+    <p>${message}</p>
+</body>
+</html>
